96e0d672bb169ccdb6bfb5373fe7843b77ccd6f0
MarcioTomaz <marciotmaz@gmail.com>
2022-08-01 15:02:50 -0300

    Add: uri 2611 with SQL and JPQL

--- uri2611/src/main/java/com/devsuperior/uri2611/Uri2611Application.java
+++ uri2611/src/main/java/com/devsuperior/uri2611/Uri2611Application.java
@@ -1,12 +1,43 @@
 package com.devsuperior.uri2611;
 
+import com.devsuperior.uri2611.DTO.MovieMinDTO;
+import com.devsuperior.uri2611.projections.MovieMinProjection;
+import com.devsuperior.uri2611.repositories.MovieRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @SpringBootApplication
-public class Uri2611Application {
+public class Uri2611Application implements CommandLineRunner {
 	
 	public static void main(String[] args) {
 		SpringApplication.run(Uri2611Application.class, args);
+	}
+
+	@Autowired
+	private MovieRepository repository;
+
+	@Override
+	public void run(String... args) throws Exception {
+
+		List<MovieMinProjection> list = repository.search1("Action");
+		List<MovieMinDTO> result1 = list.stream().map( x-> new MovieMinDTO(x)).collect(Collectors.toList());
+
+		System.out.println("\n**** Resultado SQL NORMAL: ");
+		for (MovieMinDTO obj : result1 ){
+			System.out.println(obj);
+		}
+
+		List<MovieMinDTO> result2 = repository.search2("Action");
+
+		System.out.println("\n**** Resultado SQL JPQL: ");
+		for (MovieMinDTO obj : result2 ){
+			System.out.println(obj);
+		}
+
 	}
 }
